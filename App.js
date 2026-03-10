fa196570068a8e403b0b1b784486fe095757e17b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-10 19:11:02 -0300

    melhorando layout

--- App.js
+++ App.js
@@ -4,75 +4,89 @@ export default function App() {
   const usuario = {
     nome: "Bruno Leão",
     bio: "Analista de Dados 💻",
-    seguidores: "69M",
+    seguidores: "69k",
     info_facul: "Engenharia de Software - 3º Ano",
     frase: "A vida é sobre tentar até conseguir",
+    linkedin: "https://br.linkedin.com/in/bruno-carneiro-le%C3%A3o-9a6498248",
+    github: "https://github.com/brnleao",
     avatar: "https://blog.futfanatics.com.br/wp-content/uploads/2025/08/cristiano-ronaldo-no-manchester-united-1.jpg",
   };
+
   return (
     <View style={styles.container}>
-      {/* Avatar */}
-      <Image
-        source={{ uri: usuario.avatar }}
-        style={styles.avatar}
-      />
-      {/* Nome */}
+      <Image source={{ uri: usuario.avatar }} style={styles.avatar} />
+
       <Text style={styles.nome}>{usuario.nome}</Text>
-      {/* Bio */}
       <Text style={styles.bio}>{usuario.bio}</Text>
-      {/* Informações Faculdade */}
+
       <Text style={styles.info_facul}>{usuario.info_facul}</Text>
-      {/* Stats */}
+
       <View style={styles.stats}>
         <Text style={styles.stat}>👥 {usuario.seguidores} seguidores</Text>
       </View>
 
-      {/* Frase */}
       <Text style={styles.frase}>{usuario.frase}</Text>
+
+      <Text style={styles.}>{usuario.}</Text>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#0a0a0a',
     padding: 20,
   },
+
   avatar: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
+    width: 150,
+    height: 150,
+    borderRadius: 999,
     borderWidth: 3,
     borderColor: '#E1306C',
     marginBottom: 16,
   },
+
   nome: {
     fontSize: 22,
     fontWeight: 'bold',
     color: '#fff',
     marginBottom: 8,
   },
+
   bio: {
     fontSize: 14,
     color: '#aaa',
     textAlign: 'center',
     marginBottom: 16,
   },
+
   info_facul: {
     fontSize: 14,
     color: 'yellow',
     paddingBottom: 15,
   },
+
   stats: {
     backgroundColor: '#1a1a1a',
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 20,
   },
+
   stat: {
     color: '#fff',
     fontSize: 14,
+  },
+
+  frase: {
+    marginTop: "auto",
+    color: "gray",
+    fontFamily: "serif",
+    fontSize: 14,
+    padding: 10,
+    fontStyle: 'italic',
   },
 });
